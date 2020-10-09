473f356c9aaec09a0f963e0c47f388df5c277672
Dm3583 <pankovdm@gmail.com>
2020-10-09 04:51:41 -0700

    upDate

--- js/goods.js
+++ js/goods.js
@@ -14,18 +14,26 @@ goodsRoot.appendChild(goodsList);
 
 const choosenList = document.createElement('ul');
 goodsRoot.appendChild(choosenList);
+choosenList.classList = 'choosen-items-list';
 
-const btnPrototype = function(name) {
+const choosenGoods = moduleCart.items;
+
+// for(const choosenGoods.name  )
+
+console.log(choosenGoods);
+
+const addItem = function() {
+    const choosenItemName = this.parentNode.id;
+    console.log(this.parentNode.id);
+    const choosenItem = document.createElement('li');
+    choosenItem.innerText = choosenItemName;
+    choosenList.appendChild(choosenItem);
+};
+
+const btnPrototype = function(name, callback) {
     const button = document.createElement('button');
     button.innerText = name;
-    button.onclick = function() {
-        const choosenItemName = this.parentNode.id;
-        console.log(this.parentNode.id);
-        const choosenItem = document.createElement('li');
-        choosenItem.innerText = choosenItemName;
-        choosenList.appendChild(choosenItem);
-    };
-
+    button.onclick = callback;
     return button;
 };
 
@@ -33,10 +41,11 @@ const showItems = function(products) {
     for (const item of products) {
         const listItem = document.createElement('li');
         listItem.id = item.name;
-        const btn = btnPrototype('Add item');
+        const btnAddItem = btnPrototype('Add item', addItem);
+
         listItem.innerText = item.name;
         goodsList.appendChild(listItem);
-        listItem.appendChild(btn);
+        listItem.appendChild(btnAddItem);
     }
 };
 
